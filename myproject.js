578ba1d3489ebcdc8e71af161e3508310e677ed9
donatus <natus2004@yahoo.com>
2022-05-08 01:22:02 +0000

    commiting my portfolio code

---
+++ myproject.js
@@ -0,0 +1,6 @@
+
+var hamburgerBar = document.getElementById("hamburgerBar");
+hamburgerBar.addEventListener("click", function () {
+  console.log(" site under construction");
+document.getElementById("navList").classList.toggle("active");
+})
